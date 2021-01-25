1555ffa1a9885f969bd3881892ba3f84f9e494c6
Огурчик <sergeyazarov1006@gmail.com>
2021-01-25 18:09:09 +0200

    Добавьте файлы проекта.

---
+++ Lab6.cpp
@@ -0,0 +1,12 @@
+﻿// Lab6.cpp : Определяет точку входа для приложения.
+//
+
+#include <Windows.h>
+#include <commctrl.h>
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow) 
+{
+
+}
+
+
