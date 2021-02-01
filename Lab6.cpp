5589d73309e68c7fe13a1b85f40fd511dd778bae
Огурчик <sergeyazarov1006@gmail.com>
2021-02-01 16:05:44 +0200

    Добавил сохранение/открытие, и кнопки для рисования

--- Lab6.cpp
+++ Lab6.cpp
@@ -5,38 +5,65 @@
 #include <commctrl.h>
 #include "Lab6.h"
 #include "framework.h"
+#include <assert.h>
+
+
 
 
 #pragma comment(lib, "Comctl32.lib")
 constexpr INT IDM_NEW = 100;
-#define IDM_OPEN 101
-#define IDM_SAVE 102
+constexpr INT IDM_OPEN = 101;
+constexpr INT IDM_SAVE = 102;
+constexpr INT IDM_RECT = 103;
+constexpr INT IDM_LINE = 104;
+constexpr INT IDM_CIRCLE = 105;
 
 
-
+//основные функции
 LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
 INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+HWND CreateSimpleToolbar(HWND hWndParent);
 
-
+//сохранение файла
+void CreateBMPFile(LPCWSTR pszFile, HBITMAP hBMP);
+PBITMAPINFO CreateBitmapInfoStruct(HBITMAP hBmp);
 void Save(HWND hWnd);
+//открытие файла
 void Open(HWND hWnd);
+
+//рисование объектов
+void DrawRectangle(HDC, INT left, INT right, INT top, INT bottom);
+
+
+
+static int xPosOld;
+static int yPosOld;
+static int yPosNew;
+static int xPosNew;
+static HWND ToolBar;
 
 
 const int ImageListID = 0;
 const DWORD buttonStyles = BTNS_AUTOSIZE;
+const INT bSize = 45;
+
 
 TBBUTTON tbButtons[] =
 {
     { MAKELONG(STD_FILENEW,  ImageListID), IDM_NEW,  TBSTATE_ENABLED, buttonStyles, {0}, 0, reinterpret_cast<INT_PTR>(_T("New"))},
     { MAKELONG(STD_FILEOPEN, ImageListID), IDM_OPEN, TBSTATE_ENABLED, buttonStyles, {0}, 0, reinterpret_cast<INT_PTR>(_T("Open"))},
     { MAKELONG(STD_FILESAVE, ImageListID), IDM_SAVE, TBSTATE_ENABLED, buttonStyles, {0}, 0, reinterpret_cast<INT_PTR>(_T("Save"))},
-    { MAKELONG(STD_DELETE,   ImageListID), IDM_EXIT, TBSTATE_ENABLED,buttonStyles, {0}, 0, reinterpret_cast<INT_PTR>(_T("Exit"))}
+    { MAKELONG(STD_DELETE,   ImageListID), IDM_EXIT, TBSTATE_ENABLED,buttonStyles, {0}, 0, reinterpret_cast<INT_PTR>(_T("Exit"))},
+    {MAKELONG(NULL,ImageListID),IDM_RECT,TBSTATE_ENABLED,buttonStyles, {0}, 0, reinterpret_cast<INT_PTR>(_T("Rectangle"))},
+    {MAKELONG(NULL,ImageListID),IDM_LINE,TBSTATE_ENABLED,buttonStyles, {0}, 0, reinterpret_cast<INT_PTR>(_T("Line"))},
+    {MAKELONG(NULL,ImageListID),IDM_CIRCLE,TBSTATE_ENABLED,buttonStyles, {0}, 0, reinterpret_cast<INT_PTR>(_T("Circle"))}
+
 };
 HIMAGELIST g_hImageList = NULL;
 HINSTANCE hInst;
 
 
-HWND CreateSimpleToolbar(HWND hWndParent);
 
 
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, PSTR szCmdLine, int iCmdShow)
@@ -48,7 +75,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, PSTR szCmdLine, int
     wc.cbSize = sizeof(wc);
     wc.style = CS_HREDRAW | CS_VREDRAW;
     wc.lpfnWndProc = WndProc;
-    wc.lpszMenuName = NULL;
+    wc.lpszMenuName = MAKEINTRESOURCEW(IDC_LAB6);;
     wc.lpszClassName = szClassName;
     wc.cbWndExtra = NULL;
     wc.cbClsExtra = NULL;
@@ -83,40 +110,66 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInstance, PSTR szCmdLine, int
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-    HWND ToolBar;
+    HDC hDC;
+    
+
     switch (uMsg)
     {
     case WM_CREATE:
         ToolBar = CreateSimpleToolbar(hWnd);
-        
         break;
     case WM_COMMAND:
     {
         switch (LOWORD(wParam))
         {
+        case IDM_ABOUT:
+            DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+            break;
         case IDM_NEW:
             InvalidateRect(hWnd, NULL, TRUE);
             break;
         case IDM_OPEN:
+            InvalidateRect(hWnd, NULL, TRUE);
             Open(hWnd);
             break;
         case IDM_SAVE:
+            ShowWindow(ToolBar, SW_HIDE);
             Save(hWnd);
+            ShowWindow(ToolBar, TRUE);
             break;
         case IDM_EXIT:
-            PostQuitMessage(0);
+            DestroyWindow(hWnd);
             break;
        }
     }
+    /*case WM_LBUTTONDOWN:
+        xPosOld = LOWORD(lParam);
+        yPosOld = HIWORD(lParam);
+        InvalidateRect(hWnd, NULL, TRUE);
+        break;
+   /*case WM_MOUSEMOVE:
+        xPosNew = xPosOld;
+        yPosNew = yPosOld;
+        if (WM_LBUTTONDOWN & wParam)
+        {
+            xPosNew = LOWORD(lParam);
+            yPosNew = HIWORD(lParam);
+            InvalidateRect(hWnd, NULL, TRUE);
+        }*/
     break;
+       
     case WM_PAINT:
     {
         PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hWnd, &ps);
+        HDC hDC = BeginPaint(hWnd, &ps);
+        DrawRectangle(hDC, xPosOld, yPosNew, yPosOld, yPosNew);
+
         EndPaint(hWnd, &ps);
         break;
     }
-    break;
+    case WM_SIZE:
+        SendMessage(ToolBar, TB_AUTOSIZE, 0, 0);
+        break;
     case WM_DESTROY:
         PostQuitMessage(0);
         break;
@@ -129,10 +182,10 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 HWND CreateSimpleToolbar(HWND hWndParent)
 {
-    const int numButtons = 4;
+    const int numButtons = 7;
     const int bitmapSize = 16;
-    HWND hWndToolbar = CreateWindowEx(0, TOOLBARCLASSNAME, NULL,
-        WS_CHILD | TBSTYLE_WRAPABLE, 0, 0, 0, 0,
+    HWND hWndToolbar = CreateWindowExW(WS_EX_WINDOWEDGE, TOOLBARCLASSNAME, NULL,
+        WS_CHILD | TBSTYLE_WRAPABLE, bSize, bSize, bSize, bSize,
         hWndParent, NULL, hInst, NULL);
     if (hWndToolbar == NULL)
         return NULL;
@@ -149,85 +202,51 @@ HWND CreateSimpleToolbar(HWND hWndParent)
     SendMessage(hWndToolbar, TB_ADDBUTTONS, static_cast<WPARAM>(numButtons), reinterpret_cast<LPARAM>(&tbButtons));
     SendMessage(hWndToolbar, TB_AUTOSIZE, 0, 0);
     ShowWindow(hWndToolbar, TRUE);
-
     return hWndToolbar;
 }
 
 
 void Save(HWND hWnd)
 {
-    RECT windowRect;
-    GetClientRect(hWnd, &windowRect);
+    HDC hdc[2];
+    HBITMAP hbitmap;
+    RECT rect;
+    GetClientRect(hWnd, &rect);
+    hdc[0] = GetDC(hWnd);
+    hbitmap = CreateCompatibleBitmap(hdc[0], rect.right, rect.bottom);
+    hdc[1] = CreateCompatibleDC(hdc[0]);
+    SelectObject(hdc[1], hbitmap);
 
-    int bitmap_dx = windowRect.right - windowRect.left;
-    int bitmap_dy = windowRect.bottom - windowRect.top;
-
-    BITMAPINFOHEADER bmpInfoHeader;
-    BITMAPFILEHEADER bmpFileHeader;
-    BITMAP* pBitmap;
-
-    bmpFileHeader.bfType = 0x4d42;
-    bmpFileHeader.bfSize = 0;
-    bmpFileHeader.bfReserved1 = 0;
-    bmpFileHeader.bfReserved2 = 0;
-    bmpFileHeader.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER);
-
-    bmpInfoHeader.biSize = sizeof(bmpInfoHeader);
-    bmpInfoHeader.biWidth = bitmap_dx;
-    bmpInfoHeader.biHeight = bitmap_dy;
-    bmpInfoHeader.biPlanes = 1;
-    bmpInfoHeader.biBitCount = 24;
-    bmpInfoHeader.biCompression = BI_RGB;
-    bmpInfoHeader.biSizeImage = bitmap_dx * bitmap_dy * (24 / 8);
-    bmpInfoHeader.biXPelsPerMeter = 0;
-    bmpInfoHeader.biYPelsPerMeter = 0;
-    bmpInfoHeader.biClrUsed = 0;
-    bmpInfoHeader.biClrImportant = 0;
-
-    BITMAPINFO info;
-    info.bmiHeader = bmpInfoHeader;
-
-    BYTE* memory;
-    HDC winDC = GetDC(hWnd);
-    HDC bmpDC = CreateCompatibleDC(winDC);
-   
-    HBITMAP bitmap = CreateDIBSection(winDC, &info, DIB_RGB_COLORS, (void**)&memory, NULL, 0);
-    SelectObject(bmpDC, bitmap);
-    BitBlt(bmpDC, 0, 0, bitmap_dx, bitmap_dy, winDC, 0, 0, SRCCOPY);
-    ReleaseDC(hWnd, winDC);
-
-    OPENFILENAME ofn;
-    wchar_t strFilter[] = TEXT("*.bmp");
-    wchar_t strFileName[MAX_PATH] = TEXT("");
-    memset(&ofn, 0, sizeof(OPENFILENAME));
-    ofn.lStructSize = sizeof(OPENFILENAME);
-    ofn.hwndOwner = hWnd;
-    ofn.lpstrFilter = strFilter;
-    ofn.lpstrFile = strFileName;
-    ofn.nMaxFile = MAX_PATH;
-    ofn.lpstrInitialDir = NULL;
-    ofn.Flags = OFN_SHOWHELP | OFN_OVERWRITEPROMPT;
-    wcsstr(strFileName, static_cast<LPWSTR>(ofn.lpstrFile));
-    GetSaveFileName(&ofn); 
-
-    HANDLE hFile = CreateFile(
-        ofn.lpstrFile, GENERIC_WRITE,
-        0, NULL, OPEN_ALWAYS,
-        FILE_ATTRIBUTE_NORMAL, NULL);
-
-    if (hFile == INVALID_HANDLE_VALUE)
-        return;
-
-    DWORD dwWritten = 0;
-    WriteFile(hFile, &bmpFileHeader, sizeof(BITMAPFILEHEADER), &dwWritten, NULL);
-    WriteFile(hFile, &bmpInfoHeader, sizeof(BITMAPINFOHEADER), &dwWritten, NULL);
-    WriteFile(hFile, memory, bmpInfoHeader.biSizeImage, &dwWritten, NULL);
-    CloseHandle(hFile);
+    BitBlt(
+        hdc[1],
+        rect.left,
+        rect.top-bSize,
+        rect.right,
+        rect.bottom,
+        hdc[0],
+        0,
+        0,
+        SRCCOPY
+    );
+    LPCWSTR fileName = TEXT("Bitmap.bmp");
+    CreateBMPFile(fileName, hbitmap);
 }
 
 
+void DrawRectangle(HDC hDC, INT left, INT right, INT top, INT bottom)
+{
+    Rectangle(hDC, left, top, right, bottom);
+
+}
+
 void Open(HWND hWnd)
 {
+    INT xWinPos;
+    INT yWinPos;
+    RECT rect;
+    GetClientRect(hWnd, &rect);
+    xWinPos = rect.left;
+    yWinPos = rect.top+ bSize;
     TCHAR szFile[260];
     OPENFILENAME ofn;
     ZeroMemory(&ofn, sizeof(ofn));
@@ -243,21 +262,178 @@ void Open(HWND hWnd)
     ofn.lpstrInitialDir = NULL;
     ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST;
     GetOpenFileName(&ofn);
-    if (ofn.lpstrFile == TEXT("\0")) { return; }
+    if (ofn.lpstrFile == _T("\0")) { return; }
     PAINTSTRUCT ps;
     HBITMAP hBitmap;
     BITMAP bm;
     HDC hDC;
     HDC hMemDC;
-    hDC = BeginPaint(hWnd, &ps);
+    hDC = GetDC(hWnd);
     hMemDC = CreateCompatibleDC(hDC);
     hBitmap = (HBITMAP)LoadImage(NULL, ofn.lpstrFile, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
     GetObject(hBitmap, sizeof(BITMAP), &bm);
     SelectObject(hMemDC, hBitmap);
-    BitBlt(hDC, 0, 0, bm.bmWidth, bm.bmHeight, hMemDC, 0, 0, SRCCOPY);
+    BitBlt(hDC, xWinPos, yWinPos, bm.bmWidth, bm.bmHeight, hMemDC, 0, 0, SRCCOPY);
     DeleteDC(hMemDC);
     ReleaseDC(hWnd, hDC);
     DeleteObject(hBitmap);
-    EndPaint(hWnd, &ps);
+    ReleaseDC(hWnd, hDC);
     UpdateWindow(hWnd);
+}
+
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+
+
+PBITMAPINFO CreateBitmapInfoStruct(HBITMAP hBmp)
+{
+    BITMAP bmp;
+    PBITMAPINFO pbmi;
+    WORD    cClrBits;
+
+    // Retrieve the bitmap color format, width, and height.  
+    assert(GetObject(hBmp, sizeof(BITMAP), (LPSTR)&bmp));
+
+    // Convert the color format to a count of bits.  
+    cClrBits = (WORD)(bmp.bmPlanes * bmp.bmBitsPixel);
+    if (cClrBits == 1)
+        cClrBits = 1;
+    else if (cClrBits <= 4)
+        cClrBits = 4;
+    else if (cClrBits <= 8)
+        cClrBits = 8;
+    else if (cClrBits <= 16)
+        cClrBits = 16;
+    else if (cClrBits <= 24)
+        cClrBits = 24;
+    else cClrBits = 32;
+
+    // Allocate memory for the BITMAPINFO structure. (This structure  
+    // contains a BITMAPINFOHEADER structure and an array of RGBQUAD  
+    // data structures.)  
+
+    if (cClrBits < 24)
+        pbmi = (PBITMAPINFO)LocalAlloc(LPTR,
+            sizeof(BITMAPINFOHEADER) +
+            sizeof(RGBQUAD) * (1 << cClrBits));
+
+    // There is no RGBQUAD array for these formats: 24-bit-per-pixel or 32-bit-per-pixel 
+
+    else
+        pbmi = (PBITMAPINFO)LocalAlloc(LPTR,
+            sizeof(BITMAPINFOHEADER));
+
+    // Initialize the fields in the BITMAPINFO structure.  
+
+    pbmi->bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+    pbmi->bmiHeader.biWidth = bmp.bmWidth;
+    pbmi->bmiHeader.biHeight = bmp.bmHeight;
+    pbmi->bmiHeader.biPlanes = bmp.bmPlanes;
+    pbmi->bmiHeader.biBitCount = bmp.bmBitsPixel;
+    if (cClrBits < 24)
+        pbmi->bmiHeader.biClrUsed = (1 << cClrBits);
+
+    // If the bitmap is not compressed, set the BI_RGB flag.  
+    pbmi->bmiHeader.biCompression = BI_RGB;
+
+    // Compute the number of bytes in the array of color  
+    // indices and store the result in biSizeImage.  
+    // The width must be DWORD aligned unless the bitmap is RLE 
+    // compressed. 
+    pbmi->bmiHeader.biSizeImage = ((pbmi->bmiHeader.biWidth * cClrBits + 31) & ~31) / 8
+        * pbmi->bmiHeader.biHeight;
+    // Set biClrImportant to 0, indicating that all of the  
+    // device colors are important.  
+    pbmi->bmiHeader.biClrImportant = 0;
+    return pbmi;
+}
+
+void CreateBMPFile(LPCWSTR pszFile, HBITMAP hBMP)
+{
+    HANDLE hf;                 // file handle  
+    BITMAPFILEHEADER hdr;       // bitmap file-header  
+    PBITMAPINFOHEADER pbih;     // bitmap info-header  
+    LPBYTE lpBits;              // memory pointer  
+    DWORD dwTotal;              // total count of bytes  
+    DWORD cb;                   // incremental count of bytes  
+    BYTE* hp;                   // byte pointer  
+    DWORD dwTmp;
+    PBITMAPINFO pbi;
+    HDC hDC;
+
+    hDC = CreateCompatibleDC(GetWindowDC(GetDesktopWindow()));
+    SelectObject(hDC, hBMP);
+
+    pbi = CreateBitmapInfoStruct(hBMP);
+
+    pbih = (PBITMAPINFOHEADER)pbi;
+    lpBits = (LPBYTE)GlobalAlloc(GMEM_FIXED, pbih->biSizeImage);
+
+    assert(lpBits);
+
+    // Retrieve the color table (RGBQUAD array) and the bits  
+    // (array of palette indices) from the DIB.  
+    assert(GetDIBits(hDC, hBMP, 0, (WORD)pbih->biHeight, lpBits, pbi,
+        DIB_RGB_COLORS));
+
+    // Create the .BMP file.  
+    hf = CreateFile(pszFile,
+        GENERIC_READ | GENERIC_WRITE,
+        (DWORD)0,
+        NULL,
+        CREATE_ALWAYS,
+        FILE_ATTRIBUTE_NORMAL,
+        (HANDLE)NULL);
+    assert(hf != INVALID_HANDLE_VALUE);
+
+    hdr.bfType = 0x4d42;        // 0x42 = "B" 0x4d = "M"  
+    // Compute the size of the entire file.  
+    hdr.bfSize = (DWORD)(sizeof(BITMAPFILEHEADER) +
+        pbih->biSize + pbih->biClrUsed
+        * sizeof(RGBQUAD) + pbih->biSizeImage);
+    hdr.bfReserved1 = 0;
+    hdr.bfReserved2 = 0;
+
+    // Compute the offset to the array of color indices.  
+    hdr.bfOffBits = (DWORD)sizeof(BITMAPFILEHEADER) +
+        pbih->biSize + pbih->biClrUsed
+        * sizeof(RGBQUAD);
+
+    // Copy the BITMAPFILEHEADER into the .BMP file.  
+    assert(WriteFile(hf, (LPVOID)&hdr, sizeof(BITMAPFILEHEADER),
+        (LPDWORD)&dwTmp, NULL));
+
+    // Copy the BITMAPINFOHEADER and RGBQUAD array into the file.  
+    assert(WriteFile(hf, (LPVOID)pbih, sizeof(BITMAPINFOHEADER)
+        + pbih->biClrUsed * sizeof(RGBQUAD),
+        (LPDWORD)&dwTmp, (NULL)));
+
+    // Copy the array of color indices into the .BMP file.  
+    dwTotal = cb = pbih->biSizeImage;
+    hp = lpBits;
+    assert(WriteFile(hf, (LPSTR)hp, (int)cb, (LPDWORD)&dwTmp, NULL));
+
+    // Close the .BMP file.  
+    assert(CloseHandle(hf));
+
+    // Free memory.  
+    GlobalFree((HGLOBAL)lpBits);
+
 }
